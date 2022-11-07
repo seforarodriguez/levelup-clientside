260987299bcf23ab5c50ba0cb692d2a6c78bd0c7
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-11-07 18:47:10 -0500

    added the view for the game manager

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { GameList } from "../components/game/GameList"
 
 
 export const ApplicationViews = () => {
@@ -10,7 +11,7 @@ export const ApplicationViews = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
-                {/* Add Routes here */}
+                <Route path="/" element={<GameList />} />
             </Route>
         </Routes>
     </>
